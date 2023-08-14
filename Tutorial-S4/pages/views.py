5db90732483a0ea4c4cf77698085f23d657bc18b
Cristian Davila <cddavilag@eafit.edu.co>
2023-08-14 11:18:43 -0500

    Actividad 5

--- Tutorial-S4/pages/views.py
+++ Tutorial-S4/pages/views.py
@@ -60,13 +60,13 @@ class Product:
 
     products = [ 
 
-        {"id":"1", "name":"TV", "description":"Best TV", "price":"1'000,000,00 COP"}, 
+        {"id":"1", "name":"TV", "description":"Best TV", "price":"103"}, 
 
-        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":"2'000,000,00 COP"}, 
+        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":"101"}, 
 
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":"500,000,00 COP"}, 
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":"95"}, 
 
-        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price":"300,000,00 COP"} 
+        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price":"50"} 
 
     ] 
 
@@ -99,12 +99,14 @@ class ProductShowView(View):
                 viewData = {} 
 
                 product = Product.products[int(id)-1] 
-
+                
                 viewData["title"] = product["name"] + " - Online Store" 
 
                 viewData["subtitle"] =  product["name"] + " - Product information" 
 
                 viewData["product"] = product 
+
+                product['price'] = float(product['price'])
 
                 return render(request, self.template_name, viewData)
             else:
