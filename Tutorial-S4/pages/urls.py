9bc369aa3ef430b7a58cd53f076f29d76799a6f5
Cristian Davila <cddavilag@eafit.edu.co>
2023-08-13 23:31:26 -0500

    Products

--- Tutorial-S4/pages/urls.py
+++ Tutorial-S4/pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path 
 
-from .views import HomePageView, AboutPageView, ContactPageView
+from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView
 
 
 
@@ -8,5 +8,7 @@ urlpatterns = [
     path("", HomePageView.as_view(), name='home'),
     path("about/", AboutPageView.as_view(), name='about'),
     path("contact/", ContactPageView.as_view(), name='contact'),
-
+    path('products/', ProductIndexView.as_view(), name='index'), 
+    path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    
 ]
